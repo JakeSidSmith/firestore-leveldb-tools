55343b834426ca37b79b49fd5e6b5e0bc4f2e315
Jake 'Sid' Smith <hello@jakesidsmith.com>
2022-05-17 13:03:38 +0100

    Log process errors

--- bin/index.js
+++ bin/index.js
@@ -8,10 +8,16 @@ if (process.platform !== 'darwin') {
   process.exit(1);
 }
 
-const child = execFile(path.resolve(__dirname, '../dist/to_json'), [
-  process.argv[2] || '',
-  process.argv[3] || '',
-]);
+const child = execFile(
+  path.resolve(__dirname, '../dist/to_json'),
+  [process.argv[2] || '', process.argv[3] || ''],
+  (error) => {
+    if (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+);
 
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
